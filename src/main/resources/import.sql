bf3d4e5dadd006ed2a9e90f59cb6e433e9812df2
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-09-26 16:27:29 -0300

    Seeding Employees with auto association

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,8 @@
 INSERT INTO Positions (name) VALUES ('Operador de Manufatura');
 INSERT INTO Positions (name) VALUES ('Operador Logístico');
 INSERT INTO Positions (name) VALUES ('Preparador');
+
+INSERT INTO Employees (registration, first_name, last_name, indentification, code_area, phone, manager_id) VALUES ('14297','João','Silva','97199832052','47','995784432',null);
+INSERT INTO Employees (registration, first_name, last_name, indentification, code_area, phone, manager_id) VALUES ('15925','Maria','Torres','46458339011','47','995784432',1);
+INSERT INTO Employees (registration, first_name, last_name, indentification, code_area, phone, manager_id) VALUES ('15776','Danilo','Carrara','43328164049','47','981236345',1);
+INSERT INTO Employees (registration, first_name, last_name, indentification, code_area, phone, manager_id) VALUES ('15776','Edio','Carrara','43328164049','47','981236345',1);
