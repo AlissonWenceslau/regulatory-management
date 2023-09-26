b2a8b21a8337d758d2a48a9f708a2d800ab22c4d
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-09-26 14:45:01 -0300

    Create project

---
+++ src/main/java/com/alissw/regulatory/RegulatoryApplication.java
@@ -0,0 +1,13 @@
+package com.alissw.regulatory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegulatoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegulatoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alissw/regulatory/RegulatoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alissw.regulatory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegulatoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
