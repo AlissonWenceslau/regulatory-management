6593eafa6cfc24ac83068048d3a18ac9d7dc6d18
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-09-27 12:54:52 -0300

    Database seeding Categories

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,3 +5,7 @@ INSERT INTO Positions (name) VALUES ('Preparador');
 INSERT INTO Employees (registration, first_name, last_name, indentification, code_area, phone, manager_id) VALUES ('14297','João','Silva','97199832052','47','995784432',null);
 INSERT INTO Employees (registration, first_name, last_name, indentification, code_area, phone, manager_id) VALUES ('15925','Maria','Torres','46458339011','47','995784432',1);
 INSERT INTO Employees (registration, first_name, last_name, indentification, code_area, phone, manager_id) VALUES ('15776','Danilo','Carrara','43328164049','47','981236345',1);
+
+INSERT INTO Positions (name) VALUES ('NR11');
+INSERT INTO Positions (name) VALUES ('NR12');
+INSERT INTO Positions (name) VALUES ('NR35');
