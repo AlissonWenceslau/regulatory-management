c7f8c813b72204bb4422e1cf942b3c13180cd9cd
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-09-27 16:05:19 -0300

    Database seeding association between Employee and Position

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,10 @@
 INSERT INTO Positions (name) VALUES ('Operador de Manufatura');
 INSERT INTO Positions (name) VALUES ('Operador Logístico');
-INSERT INTO Positions (name) VALUES ('Preparador');
+INSERT INTO Positions (name) VALUES ('Supervisor');
 
-INSERT INTO Employees (registration, first_name, last_name, indentification, code_area, phone, manager_id) VALUES ('14297','João','Silva','97199832052','47','995784432',null);
-INSERT INTO Employees (registration, first_name, last_name, indentification, code_area, phone, manager_id) VALUES ('15925','Maria','Torres','46458339011','47','995784432',1);
-INSERT INTO Employees (registration, first_name, last_name, indentification, code_area, phone, manager_id) VALUES ('15776','Danilo','Carrara','43328164049','47','981236345',1);
+INSERT INTO Employees (registration, first_name, last_name, indentification, code_area, phone, manager_id, position) VALUES ('14297','João','Silva','97199832052','47','995784432',null,3);
+INSERT INTO Employees (registration, first_name, last_name, indentification, code_area, phone, manager_id, position) VALUES ('15925','Maria','Torres','46458339011','47','995784432',1,2);
+INSERT INTO Employees (registration, first_name, last_name, indentification, code_area, phone, manager_id, position) VALUES ('15776','Danilo','Carrara','43328164049','47','981236345',1,1);
 
 INSERT INTO Categories (name) VALUES ('NR11');
 INSERT INTO Categories (name) VALUES ('NR12');
