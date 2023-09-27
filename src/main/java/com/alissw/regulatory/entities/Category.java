68c36b1eb557f6d26b1746eadbf3f21a32be8f32
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-09-27 17:46:00 -0300

    Serializable Category

--- src/main/java/com/alissw/regulatory/entities/Category.java
+++ src/main/java/com/alissw/regulatory/entities/Category.java
@@ -1,5 +1,7 @@
 package com.alissw.regulatory.entities;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,8 +10,9 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Categories")
-public class Category {
-
+public class Category implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
