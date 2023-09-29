ef5c15e1b3d10cf8ed36bb2e57b7c53d97247fab
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-09-29 19:19:13 -0300

    Configuration columnDefinition

--- src/main/java/com/alissw/regulatory/entities/Registry.java
+++ src/main/java/com/alissw/regulatory/entities/Registry.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 
 import com.alissw.regulatory.entities.enums.Status;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,7 +23,9 @@ public class Registry implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant startDate;
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant endDate;
 	private Status status;
 	
