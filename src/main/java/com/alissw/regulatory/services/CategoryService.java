c043442a767e1935602685b8233f1bcf3d1c168a
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-09-29 21:31:59 -0300

    CategoryService

---
+++ src/main/java/com/alissw/regulatory/services/CategoryService.java
@@ -0,0 +1,33 @@
+package com.alissw.regulatory.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.alissw.regulatory.dto.CategoryDTO;
+import com.alissw.regulatory.entities.Category;
+import com.alissw.regulatory.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(c -> new CategoryDTO(c)).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> optional = repository.findById(id);
+		Category entity = optional.get();
+		return new CategoryDTO(entity);
+	}
+}
