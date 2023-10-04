d9b5291d983e5e578518d6079c81524e2ed3ef5d
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-10-04 01:21:05 -0300

    Refactor enum

--- backend/src/main/java/com/alissw/regulatory/entities/enums/Status.java
+++ backend/src/main/java/com/alissw/regulatory/entities/enums/Status.java
@@ -1,13 +1,6 @@
 package com.alissw.regulatory.entities.enums;
 
 public enum Status {
-
-	UP(1),
-	DOWN(2);
-
-	private Status(int i) {
-		// TODO Auto-generated constructor stub
-	}
-	
- 
+	UP,
+	DOWN;
 }
